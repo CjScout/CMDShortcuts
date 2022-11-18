1309095513a669a3b3ffc8048605b3210a095eb7
Connor Schuler <cjsbeverly@gmail.com>
2022-11-17 20:55:47 -0500

    Many new commands, will update readme soon

--- Script.bat
+++ Script.bat
@@ -4,22 +4,20 @@
 echo Select Catagory
 echo 1) Network Tools
 echo 2) Disk Tools
-echo 3) Power Tools
-echo 4) File Assosiations
-echo 5) Other Utilities
+echo 3) Other Utilities
 
 set /p "catagory=Enter Catagory #: "
-echo %catagory%
 
 IF "%catagory%"=="1" GOTO Cat1
 IF "%catagory%"=="2" GOTO Cat2
+IF "%catagory%"=="3" GOTO Cat3
 
 ::---------------------------------------
 :Cat1
 :: Display Options
 
 echo Network Tools: Select Tool
-echo 1) Display Network Information
+echo 1) Display All Network Information
 echo 2) See DNS Server
 echo 3) Flush DNS
 echo 4) Get New IP
@@ -34,9 +32,9 @@ echo 12) Turn On Windows Firewall
 echo 13) Ping IP
 echo 14) non-stop ping ip
 echo 15) Create and open a file that lists all active connections
+echo 16) Retrieve Wi-Fi password
 
 set /p "tool=Enter Tool #: "
-echo %tool%
 
 IF "%tool%"=="1" GOTO Cat1Option1
 IF "%tool%"=="2" GOTO Cat1Option2
@@ -53,15 +51,16 @@ IF "%tool%"=="12" GOTO Cat1Option12
 IF "%tool%"=="13" GOTO Cat1Option13
 IF "%tool%"=="14" GOTO Cat1Option14
 IF "%tool%"=="15" GOTO Cat1Option15
+IF "%tool%"=="16" GOTO Cat1Option16
 
 :Cat1Option1
-echo Getting Network Information...
+echo Getting All Network Information...
 ipconfig /all
 GOTO END
 
 :Cat1Option2
 echo Getting DNS Information...
-ipconfig /all | findstr DNS
+netsh interface ip show dnsservers
 GOTO END
 
 :Cat1Option3
@@ -71,13 +70,18 @@ GOTO END
 
 :Cat1Option4
 echo Getting new IP...
+echo Releasing old IP
 ipconfig /release
+echo Acquiring new IP
 ipconfig /renew
+echo New IP Address Acquired
 GOTO END
 
 :Cat1Option5
 set /p "site=Enter domain name: "
+echo Resolving Address %site%
 nslookup %site%
+echo Address Resolved
 GOTO END
 
 :Cat1Option6
@@ -86,7 +90,10 @@ tracert %site%
 GOTO END
 
 :Cat1Option7
+echo Outputing DNS Cache to command prompt and file
 ipconfig /displaydns
+ipconfig /displaydns > output.txt
+output.txt
 GOTO END
 
 :Cat1Option8
@@ -102,11 +109,15 @@ netsh interface ip show address | findstr "IP Address"
 GOTO END
 
 :Cat1Option11
+echo Turning off windows firewall
 netsh advfirewall set allprofiles state off
+echo Firewall disabled
 GOTO END
 
 :Cat1Option12
+echo Turning on windows firewall
 netsh advfirewall set allprofiles state on
+echo Firewall enabled
 GOTO END
 
 :Cat1Option13
@@ -120,23 +131,29 @@ ping -t %site%
 GOTO END
 
 :Cat1Option15
-netstat -adfo 5 | output.txt
+echo Watching for open ports
+netstat -abf 1 > output.txt
 output.txt
+GOTO END
+
+:Cat1Option16
+netsh wlan show profile
+set /p "network=Enter Network Name (Use quotes if space): "
+netsh wlan show profile name=%network% key=clear | findstr Key
 GOTO END
 
 ::---------------------------------------
 :Cat2
 :: Display Options
 
-echo Didk Tools: Select Tool
+echo Disk Tools: Select Tool
 echo 1) Check Disk
 echo 2) Check System Files
 echo 3) Check Image
 echo 4) Repair Image
-echo 5) Example
+echo 5) Format disk
 
 set /p "tool=Enter Tool #: "
-echo %tool%
 
 IF "%tool%"=="1" GOTO Cat2Option1
 IF "%tool%"=="2" GOTO Cat2Option2
@@ -163,28 +180,47 @@ DISM /Online /Cleanup-Image /RestoreHealth
 GOTO END
 
 :Cat2Option5
-echo Cat2Option5
+
+::List Disks
+echo list disk > ListDisk.txt
+echo exit >> ListDisk.txt
+diskpart /s ListDisk.txt
+
+set /p "disk=Enter Disk #: "
+
+::Format Disk
+echo select disk %disk% > FormatDisk.txt
+echo clean >> FormatDisk.txt
+echo create partition primary >> FormatDisk.txt
+echo select part 1 >> FormatDisk.txt
+echo format fs=ntfs quick >> FormatDisk.txt
+diskpart /s FormatDisk.txt
 GOTO END
 
 ::---------------------------------------
 :Cat3
 :: Display Options
 
-echo Catagory 3: Select Tool
+echo Orher Utilities: Select Tool
 echo 1) Check Power Usage
 echo 2) Check Battery
-echo 3) Example
-echo 4) Example
-echo 5) Example
+echo 3) See File Assosiations
+echo 4) Restart to BIOS
+echo 5) List all running taks
+echo 6) Kill A Task
+echo 7) See all drivers
+echo 8) Get system Info
 
 set /p "tool=Enter Tool #: "
-echo %tool%
 
 IF "%tool%"=="1" GOTO Cat3Option1
 IF "%tool%"=="2" GOTO Cat3Option2
 IF "%tool%"=="3" GOTO Cat3Option3
 IF "%tool%"=="4" GOTO Cat3Option4
 IF "%tool%"=="5" GOTO Cat3Option5
+IF "%tool%"=="6" GOTO Cat3Option6
+IF "%tool%"=="7" GOTO Cat3Option7
+IF "%tool%"=="8" GOTO Cat3Option8
 
 :Cat3Option1
 powercfg /energy
@@ -195,97 +231,32 @@ powercfg /batteryreport
 GOTO END
 
 :Cat3Option3
-echo Cat3Option3
-GOTO END
-
-:Cat3Option4
-echo Cat3Option4
-GOTO END
-
-:Cat3Option5
-echo Cat3Option5
-GOTO END
-
-::---------------------------------------
-:Cat4
-:: Display Options
-
-echo Catagory 4: Select Tool
-echo 1) See all assosiations
-echo 2) Example
-echo 3) Example
-echo 4) Example
-echo 5) Example
-
-set /p "tool=Enter Tool #: "
-echo %tool%
-
-IF "%tool%"=="1" GOTO Cat4Option1
-IF "%tool%"=="2" GOTO Cat4Option2
-IF "%tool%"=="3" GOTO Cat4Option3
-IF "%tool%"=="4" GOTO Cat4Option4
-IF "%tool%"=="5" GOTO Cat4Option5
-
-:Cat4Option1
 assoc
 GOTO END
 
-:Cat4Option2
-echo Cat4Option2
-GOTO END
-
-:Cat4Option3
-echo Cat4Option3
-GOTO END
-
-:Cat4Option4
-echo Cat4Option4
-GOTO END
-
-:Cat4Option5
-echo Cat4Option5
-GOTO END
-
-::---------------------------------------
-:Cat5
-:: Display Options
-
-echo Catagory 5: Select Tool
-echo 1) Restart to BIOS
-echo 2) List All Running Tasks
-echo 3) Kill a Task
-echo 4) Example
-echo 5) Example
-
-set /p "tool=Enter Tool #: "
-echo %tool%
-
-IF "%tool%"=="1" GOTO Cat5Option1
-IF "%tool%"=="2" GOTO Cat5Option2
-IF "%tool%"=="3" GOTO Cat5Option3
-IF "%tool%"=="4" GOTO Cat5Option4
-IF "%tool%"=="5" GOTO Cat5Option5
-
-:Cat5Option1
+:Cat3Option4
 shutdown /r /fw /f /t 0
 GOTO END
 
-:Cat5Option2
-tasklist
+:Cat3Option5
+tasklist > output.txt
+output.txt
 GOTO END
 
-:Cat5Option3
+:Cat3Option6
 set /p "pid=Enter PID: "
 taskkill /f /pid %pid%
 GOTO END
 
-:Cat5Option4
-echo Cat5Option4
+:Cat3Option7
+driverquery -v
 GOTO END
 
-:Cat5Option5
-echo Cat5Option5
+:Cat3Option8
+sysetminfo > output.txt
+output.txt
 GOTO END
 
+::---------------------------------------
 :END
 pause
